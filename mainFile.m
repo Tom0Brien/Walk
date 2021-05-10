f53fec0b5d6064d3e7d23ca1805f3e34e45bd377
Thomas O'Brien <thomas16441@gmail.com>
2021-05-10 19:38:49 +1000

    Split up plotting walking gait from data

--- mainFile.m
+++ mainFile.m
@@ -31,17 +31,39 @@ param.initialConditions = opt_joint_angles_2(:,end);
 trajectory_3 = generateFootTrajectory(param,isHalfStep);
 %% Inverse Kinematics - Half Step - support foot -> left
 [opt_joint_angles_3] = inverseKinematics(robot,trajectory_3,param);
-%% Plot first step
+%% Plot Walking
+% Plot first step
 param.supportFoot = 'left_foot';
 param.swingFoot = 'right_foot';
-plot3DRobot(opt_joint_angles_1,robot,param,trajectory_1);
-%% Plot 2nd step
+plotWalk(opt_joint_angles_1,robot,param,trajectory_1);
+% Plot 2nd step
 param.supportFoot = 'right_foot';
 param.swingFoot = 'left_foot';
-plot3DRobot(opt_joint_angles_2,robot,param,trajectory_2);
-%% Plot 3rd step
+plotWalk(opt_joint_angles_2,robot,param,trajectory_2);
+% Plot 3rd step
 param.supportFoot = 'left_foot';
 param.swingFoot = 'right_foot';
-plot3DRobot(opt_joint_angles_3,robot,param,trajectory_3);
+plotWalk(opt_joint_angles_3,robot,param,trajectory_3);
+%% Plot Data
+% Plot first step
+param.supportFoot = 'left_foot';
+param.swingFoot = 'right_foot';
+plotData(opt_joint_angles_1,robot,param,trajectory_1);
+% Plot 2nd step
+param.supportFoot = 'right_foot';
+param.swingFoot = 'left_foot';
+plotData(opt_joint_angles_2,robot,param,trajectory_2);
+% Plot 3rd step
+param.supportFoot = 'left_foot';
+param.swingFoot = 'right_foot';
+plotData(opt_joint_angles_3,robot,param,trajectory_3);
+%% Plot CoM
+plotCoM(opt_joint_angles_1,param);
+param.supportFoot = 'right_foot';
+param.swingFoot = 'left_foot';
+plotCoM(opt_joint_angles_2,param);
+param.supportFoot = 'left_foot';
+param.swingFoot = 'right_foot';
+plotCoM(opt_joint_angles_3,param);
 %% Pack servo positions
 servo_positions = [opt_joint_angles_1,opt_joint_angles_2,opt_joint_angles_3];
