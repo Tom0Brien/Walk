67c6d2654020d7b875122821b5f9e181840e126f
Thomas O'Brien <41043317+TheCoolKidsTable@users.noreply.github.com>
2021-08-30 10:46:56 +1000

    ZMP gait params
    - Work well without foot studs

--- costFunction.m
+++ costFunction.m
@@ -4,26 +4,18 @@ FK = Kinematics();
 %% Cost
 rSPp = FK.xe(q,p);
 % Torso from world
-if(p.stepCount == 1)
-    Hwp = trvec2tform([p.footstep(p.stepCount,1)  0.055 -0.46])*roty(pi/2);
-elseif (p.stepCount == 2)
-    Hwp = trvec2tform([p.footstep(p.stepCount,1)  -0.055 -0.46])*roty(pi/2);
-else
-    Hwp = trvec2tform([p.footstep(3,1)+p.footstep(2,1) 0.055 -0.46])*roty(pi/2);
-end
-        
+Hwp = trvec2tform([p.footstep(p.stepCount,1)  (-1)^(p.stepCount+1)*0.055 -0.46])*roty(pi/2);        
 % transform com from torso to support foot space
 Htp = FK.Htp(q,p);
 rCTt = FK.CoM(q,p);
 rCPp =  Htp\[rCTt;1];
 % transform com desired from world to support foot space
-rCdPp = inv(Hwp)*[rCdWw(:,i);1];
+rCdPp = Hwp\[rCdWw(:,i);1];
 
 cost = (rSdPp(:,i) - rSPp).'*(rSdPp(:,i) - rSPp) + ... % swing foot desired - swing foot
-       (rCdPp - rCPp).'*(rCdPp - rCPp);
-%    + ...
-%        0.5*trace(eye(3)-FK.R(q,p)) + ...
-%        0.5*trace(eye(3)-FK.RL(q,p));
+       (rCdPp - rCPp).'*(rCdPp - rCPp)+ ...
+       0.5*trace(eye(3)-FK.R(q,p)) + ...
+       0.5*trace(eye(3)-FK.RL(q,p));
 %    + ...        0.5*trace(eye(3)-FK.RR(q,p));
 %    + ...
 %         + ...
--- gaitParameters.m
+++ gaitParameters.m
@@ -1,10 +1,10 @@
 function param = gaitParameters(robot)
 param.num_bodies = robot.NumBodies;
-param.step_height = 0.04;
-param.step_length = 0.1;
-param.step_width = 0.13;
+param.step_height = 0.05;
+param.step_length = 0.075;
+param.step_width = 0.18;
 param.step_time = 1;
-param.Ts = 0.05;
+param.Ts = 0.01;
 param.N_samples = param.step_time/param.Ts;
 param.initial_conditions = initialConditions;
 param.robot = robot;
